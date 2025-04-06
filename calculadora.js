8a24f558af7d1f3eb79b9fde45dac62555e6aae0
Danilo-MartinsM <danilo.martins.dev@gmail.com>
2025-04-06 12:13:00 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,6 @@
-const numero1 = parseFloat(Process.argv[2]);
-const operacao = Process.argv[3];
-const numero2 = parseFloat(Process.argv[4]);
+const numero1 = parseFloat(process.argv[2]);
+const operacao = process.argv[3];
+const numero2 = parseFloat(process.argv[4]);
 let resultado;
 
 if (operacao === '+'){
@@ -15,4 +15,4 @@ if (operacao === '+'){
     console.log('Operação inválida.');
     process.exit(1);
 }
-console.log('Resultado: '+ resultado +'.');
+console.log('Resultado: '+ resultado);
